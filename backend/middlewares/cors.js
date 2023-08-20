d09b4ad9aa6e3163f23cf4b688060c75cb7bc503
Viktor <vitassablino1@gmail.com>
2023-08-20 03:01:45 +0300

    test point

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -11,10 +11,10 @@ module.exports = (req, res, next) => {
   const { origin } = req.headers;
   const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
-  if (allowedList.includes(origin)) {
+  /* if (allowedList.includes(origin)) { */
     res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Credentials', true);
-  }
+  /* } */
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
     res.header('Access-Control-Allow-Headers', requestHeaders);
