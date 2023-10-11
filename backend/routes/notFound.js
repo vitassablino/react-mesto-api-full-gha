b77b99f0515385a03ea22fb63fcf5a12df4a3838
Viktor <vitassablino1@gmail.com>
2023-10-11 23:57:42 +0300

    Wipe

--- backend/routes/notFound.js
+++ backend/routes/notFound.js
@@ -1,4 +1,5 @@
 const http2 = require('http2');
+const router = require('express').Router();
 
 router.all('*', (req, res) => {
   res.status(http2.constants.HTTP_STATUS_NOT_FOUND).send({message: "Страница не найдена"})
