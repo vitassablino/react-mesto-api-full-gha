05583dc1c0b5d8f32e4236861679a4e8551b3031
Viktor <vitassablino1@gmail.com>
2023-10-12 19:54:32 +0300

    added crashtest

--- backend/app.js
+++ backend/app.js
@@ -26,6 +26,12 @@ app.use(cors);
 
 app.use(requestLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.use('/', rootRouter);
 
 app.use(errorLogger);
